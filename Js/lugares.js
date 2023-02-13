7633824caa0785a9d34c60e4383180c4fb577062
Dylan Navarro <dylanrubennavarro@hotmail.com>
2023-02-12 21:45:38 -0300

    Selector de destino, y articulo en gestion con sass

--- Js/lugares.js
+++ Js/lugares.js
@@ -1,1 +1,35 @@
- 
+window.sr = ScrollReveal();
+
+sr.reveal(".header, .contenedor-art-mas",{
+    duration: 300,
+    origin: 'bottom',
+    distance: '0px'
+});
+
+// sr.reveal(".banner-titulo, .banner-texto",{
+//     duration: 1000,
+//     origin: 'top',
+//     distance: '-100px'
+// });
+var grande = document.querySelector('.grande-selector')
+// console.log(grande)
+let destinoSelector = document.querySelectorAll('.destino-selector')
+// console.log(destinoSelector)
+let botonDerIzq = document.querySelectorAll('.boton')
+// console.log(botonDerIzq)
+destinoSelector.forEach((destino,i)=>{
+   
+    destinoSelector[i].addEventListener('click',(e)=>{
+        
+        if(e.target.id == 'btnDerecha'){
+            var posicion = 1
+        } else if (e.target.id == 'btnIzquierda'){
+            var posicion = 0
+        }
+        // console.log(posicion)
+        var operacion = posicion * -50
+        // console.log(operacion)
+
+        grande.style.transform = `translateX(${ operacion }%)`
+    })
+})
